8d85498a4851c225813bcc6dfe0a8d66fda0d585
agnesem670 <agnesem670@gmail.com>
2023-09-25 22:08:04 +0200

    Router links and structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, accusamus.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis nostrum a placeat. Natus eos ab obcaecati dolorum quae mollitia, molestias corrupti cumque quam praesentium aliquid, distinctio minus, fugiat odio numquam! Magni soluta rerum dolor, a officiis fuga sunt error. Inventore, omnis tenetur eum dolores autem maxime quisquam porro earum optio ipsum. Laborum perferendis accusamus, perspiciatis explicabo magni quia quaerat modi consectetur officiis. Magnam nulla repellat mollitia alias, eaque aut officiis quaerat delectus error? Soluta deleniti labore est animi possimus perspiciatis reiciendis pariatur beatae commodi, corrupti at autem laborum natus accusantium odit optio repellendus minus! Provident sunt nulla, tenetur impedit nostrum beatae officia corrupti, ab vel ex voluptates porro nihil et ullam quam expedita est ipsam nisi. Ipsa delectus dolores ut saepe soluta molestiae omnis, recusandae ipsam sapiente sit fugit ullam tempore tempora sunt et qui aliquam iste. Sequi dicta vero, beatae amet fugit voluptate facere, doloribus maiores quibusdam ullam hic et illo? Quidem vitae deserunt officiis quaerat quia optio dolores veniam? Quo magnam delectus nobis dicta quos non distinctio quaerat saepe quis, consequuntur recusandae. Totam, eaque dolorum dolore veniam facere debitis suscipit necessitatibus vel corrupti, velit, distinctio minima. Corporis labore error totam saepe at dignissimos sunt consequuntur quasi et placeat.</p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>

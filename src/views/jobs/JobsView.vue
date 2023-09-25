1d716170dec4dfa87c528a6b615c94f7b5f490a5
agnesem670 <agnesem670@gmail.com>
2023-09-25 22:54:13 +0200

    Dynamic Links

--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -1,8 +1,10 @@
 <template>
-<h1>Jobs</h1>
-<div v-for="job in jobs" :key="job.id">
-<h2>{{ job.title }}</h2>
-</div>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id" class="job">
+        <router-link :to="{name: 'jobDetails', params: {id: job.id}}">
+            <h2>{{ job.title }}</h2>
+        </router-link>
+    </div>
 </template>
 
 <script>
@@ -10,13 +12,29 @@ export default {
     data() {
         return {
             jobs: [
-                {title: 'UX Designer', id: 1, details: 'lorem'},
-                {title: 'Web Developer', id: 2, details: 'lorem'},
-                {title: 'Ninja Vue Developer', id: 3, details: 'lorem'},
+                { title: 'UX Designer', id: 1, details: 'lorem' },
+                { title: 'Web Developer', id: 2, details: 'lorem' },
+                { title: 'Ninja Vue Developer', id: 3, details: 'lorem' },
             ]
         }
     }
 }
 </script>
 
-<style></style>
+<style>
+.job h2 {
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+}
+.job h2:hover {
+    background: #ddd;
+}
+.job a{
+    text-decoration: none;
+}
+</style>

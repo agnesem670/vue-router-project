361dd12a478a67b5e5c5ffe96eca7f1ea7592fb6
agnesem670 <agnesem670@gmail.com>
2023-09-26 16:08:28 +0200

    Refs vs reactive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>My name is {{ name }} my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+    <h2>Refs</h2>
+    <p>{{ personalInfo.name }} - {{ personalInfo.age }}</p>
+    <button @click="updateInfo">Update info One</button>
+    <h2>Reactive</h2>
+    <p>{{ personalTwo.name }} - {{ personalTwo.age }}</p>
+    <button @click="updateInfoTwo">Update info Two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-   
-    const name = ref('mario')
-    const age = ref(30)
 
-    const handleClick = () => {
-      name.value = 'luigi'
-      age.value = 35
+    const personalInfo = ref({ name: 'aga', age: 39 })
+    const personalTwo = reactive({name: 'ela', age: 35})
+
+    const updateInfo = () => {
+      personalInfo.value.age = 40
     }
 
-    return { name, age, handleClick}
+     const updateInfoTwo = () => {
+      personalTwo.age = 55
+    }
+
+    return { personalInfo, updateInfo, personalTwo, updateInfoTwo }
   }
 
 }

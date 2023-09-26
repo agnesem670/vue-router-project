0ecbe6f4656225fef78afdf473cd51f113346f82
agnesem670 <agnesem670@gmail.com>
2023-09-26 13:09:44 +0200

    setup function and template refs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p ref="p">My name is {{ name }} my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    console.log(this)
+
+    const p = ref(null)
+
+    let name = 'mario'
+    let age = 30
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello'
+    }
+
+    return { name, age, handleClick, p}
   }
+
 }
 </script>

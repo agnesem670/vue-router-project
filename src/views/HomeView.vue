ab6318222731fdea2c9d5a391ad95612fd402602
agnesem670 <agnesem670@gmail.com>
2023-09-27 12:44:14 +0200

    fetching data in setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts" />
-    <button @click="showPosts = !showPosts">toggle posts</button>
-    <button @click="posts.pop()">delete post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading ...</div>
+
+
   </div>
 </template>
 
@@ -15,22 +19,25 @@ export default {
   name: 'HomeView',
   components: { PostList },
   setup() {
-    const posts = ref([
-      {
-        title: 'welcome to the blog',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere vitae dolore rem at a aut magni dicta, nisi quos laborum. Expedita delectus, excepturi quasi nulla dolorem iste optio labore nobis tempora blanditiis alias, at rem numquam fugiat ducimus molestiae ex ipsam laborum quia? Dignissimos animi fugit voluptas eveniet beatae!',
-        id: 1
-      },
-      {
-        title: 'top 5 css tips',
-        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere vitae dolore rem at a aut magni dicta, nisi quos laborum. Expedita delectus, excepturi quasi nulla dolorem iste optio labore nobis tempora blanditiis alias, at rem numquam fugiat ducimus molestiae ex ipsam laborum quia? Dignissimos animi fugit voluptas eveniet beatae!',
-        id: 2
-      },
-    ]);
-    const showPosts = ref(true)
-    return { posts, showPosts };
-  },
-  components: { PostList }
+    const posts = ref([]);
+    const error = ref(null)
+
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+        }
+        posts.value = await data.json()
+      }
+      catch (err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
+    load()
+    return { posts, error };
+  }
 }
 </script>
 

65c133b7c7e4e4a23ec40f1ade62567bbbb73acb
agnesem670 <agnesem670@gmail.com>
2023-09-27 11:00:50 +0200

    using props in setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,41 +1,34 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
-    <button @clicl="handleClick">stop watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { ref, computed, watch, watchEffect } from 'vue';
+import PostList from '@/components/PostList.vue';
+import { ref } from 'vue';
 
 export default {
   name: 'HomeView',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'yoshi', 'luigi', 'toad', 'bowser', 'koopa', 'peach'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watch effect ran', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-
-    return { names, search, matchingNames, handleClick }
-  }
-
+    const posts = ref([
+      {
+        title: 'welcome to the blog',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere vitae dolore rem at a aut magni dicta, nisi quos laborum. Expedita delectus, excepturi quasi nulla dolorem iste optio labore nobis tempora blanditiis alias, at rem numquam fugiat ducimus molestiae ex ipsam laborum quia? Dignissimos animi fugit voluptas eveniet beatae!',
+        id: 1
+      },
+      {
+        title: 'top 5 css tips',
+        body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit facere vitae dolore rem at a aut magni dicta, nisi quos laborum. Expedita delectus, excepturi quasi nulla dolorem iste optio labore nobis tempora blanditiis alias, at rem numquam fugiat ducimus molestiae ex ipsam laborum quia? Dignissimos animi fugit voluptas eveniet beatae!',
+        id: 2
+      },
+    ]);
+    return { posts };
+  },
+  components: { PostList }
 }
 </script>
+
+

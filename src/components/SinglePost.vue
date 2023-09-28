e620678a2a51628d070c243bb9ec74dc179c641a
agnesem670 <agnesem670@gmail.com>
2023-09-28 22:26:49 +0200

    composables

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,6 +1,11 @@
 <template>
-<h3>{{ post.title }}</h3>
-<p>{{ snippet }}</p>
+    <router-link :to="{name: 'details', params: {id: post.id}}">
+        <h3>{{ post.title }}</h3>
+    </router-link>
+    <p>{{ snippet }}</p>
+    <span v-for="tag in post.tags" :key="tag">
+        #{{ tag }}
+    </span>
 </template>
 
 <script>
@@ -8,16 +13,14 @@ import { computed } from 'vue'
 
 export default {
     props: ['post'],
-    setup(props){
+    setup(props) {
         const snippet = computed(() => {
             return props.post.body.substring(0, 100) + '....'
         })
 
-        return { snippet}
+        return { snippet }
     }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
